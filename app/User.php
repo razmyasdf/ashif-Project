cbb312921f97018d6cc2b362c8feb00c5f142f0f
Razmy Mahsoom <razmyasdf@gmail.com>
2019-10-21 22:27:50 +0530

    Update User.php

--- app/User.php
+++ app/User.php
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function vehicle(){
+
+        return $this->hasMany('App\vehicle');
+    }
 }
