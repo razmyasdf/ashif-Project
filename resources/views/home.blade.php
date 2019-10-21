68dc1a91e54a20ff17001471fd07c473178df7c2
Razmy Mahsoom <razmyasdf@gmail.com>
2019-10-21 14:23:27 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,67 @@
                             {{ session('status') }}
                         </div>
                     @endif
+                    <div class="card card-primary">
+                            <div class="card-header">
 
-                    You are logged in!
+                            </div>
+                                <!-- /.card-header -->
+                                <div class="card-body">
+                                        <div class="form-group row">
+                                                <label for="inputPassword3" class="col-sm-2 col-form-label">Vehicle</label>
+                                                <div class="col-sm-10">
+                                                    <input type="text" class="form-control" id="date" name="vehicle" value="" placeholder="vehicle chassis number">
+                                                    <button type="submit" class="btn btn-primary">Add</button>
+                                                </div>
+                                            </div>
+
+
+                                            <table id="example2" class="table table-bordered table-light">
+                                                    <thead class="thead-dark">
+                                                         <tr>
+                                                            <th>Vehicle Chassis Number</th>
+
+                                                            <th></th>
+
+                                                    <tbody>
+
+                                                            <tr>
+
+                                                            <td>cc1152d588</td>
+
+                                                            <td>
+                                                            <form method="GET" action="">
+                                                                @csrf
+                                                                <button type="sumbit" class="btn btn-block btn-outline-success btn-xs" >Edit</button>
+                                                            </form>
+
+                                                            <form action="" method="POST">
+                                                                @csrf
+                                                                <button type="sumbit" class="btn btn-block btn-outline-success btn-xs" >Add Image</button>
+                                                            </form>
+                                                            <form action="" method="POST">
+                                                                    @csrf
+                                                                    <button type="sumbit" class="btn btn-block btn-outline-success btn-xs" >View</button>
+                                                                </form>
+                                                            </td>
+
+                                                            </tr>
+
+
+                                                    </tbody>
+                                                </table>
+                                                        </tr>
+                                                    </thead>
+
+
+                                </div>
+                    </div>
+
+
+
+
+
+
                 </div>
             </div>
         </div>
