f519292ee25d5cb523ab4c7c4a59110067d29a9d
Razmy Mahsoom <razmyasdf@gmail.com>
2019-10-21 22:27:45 +0530

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,13 +19,18 @@
                             </div>
                                 <!-- /.card-header -->
                                 <div class="card-body">
+                                    <form action="/vehicle/create" method="POST">
+                                        @csrf
                                         <div class="form-group row">
                                                 <label for="inputPassword3" class="col-sm-2 col-form-label">Vehicle</label>
                                                 <div class="col-sm-10">
-                                                    <input type="text" class="form-control" id="date" name="vehicle" value="" placeholder="vehicle chassis number">
+                                                    <input type="text" class="form-control" id="vcn" name="vcn" value="" placeholder="vehicle chassis number" required>
                                                     <button type="submit" class="btn btn-primary">Add</button>
                                                 </div>
                                             </div>
+
+
+                                    </form>
 
 
                                             <table id="example2" class="table table-bordered table-light">
@@ -37,29 +42,21 @@
 
                                                     <tbody>
 
-                                                            <tr>
 
-                                                            <td>cc1152d588</td>
+                                                                @foreach (auth()->user()->vehicle as $item)
 
+                                                                <tr>
+                                                                    <td>{{$item->vcn}}</td>
                                                             <td>
-                                                            <form method="GET" action="">
+                                                            <form method="GET" action="/show/{{$item->vcn}}">
                                                                 @csrf
-                                                                <button type="sumbit" class="btn btn-block btn-outline-success btn-xs" >Edit</button>
+                                                                <button type="sumbit" class="btn btn-block btn-outline-success btn-xs" >View</button>
                                                             </form>
-
-                                                            <form action="" method="POST">
-                                                                @csrf
-                                                                <button type="sumbit" class="btn btn-block btn-outline-success btn-xs" >Add Image</button>
-                                                            </form>
-                                                            <form action="" method="POST">
-                                                                    @csrf
-                                                                    <button type="sumbit" class="btn btn-block btn-outline-success btn-xs" >View</button>
-                                                                </form>
                                                             </td>
 
                                                             </tr>
 
-
+                                                            @endforeach
                                                     </tbody>
                                                 </table>
                                                         </tr>
