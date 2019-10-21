0a830953466de4d50ff2851f106e840a37e2ed63
Razmy Mahsoom <razmyasdf@gmail.com>
2019-10-21 22:27:41 +0530

    Create show.blade.php

---
+++ resources/views/show.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+<div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                <div class="card-header bg-primary"><h1>{{$vehicle->vcn}}</h1></div>
+
+                    <div class="card-body">
+                    <form action="/image/add/{{$vehicle->vcn}}" class="form-group" method="POST" enctype="multipart/form-data">
+                        @csrf
+                                <label for="">Add Image for Vehicle</label>
+                                <input type="file" name="img" class="form-control-file" id="img" required>
+
+                                <button type="submit" class="btn btn-primary">add</button>
+                            </form>
+
+                            <div class="row pt-5">
+                                    @foreach ($vehicle->image as $item)
+                                <div class="col-4 pt-3">
+                                <img src="/storage/{{$item->img}}" class="w-100" alt="">
+                                </div>
+                                @endforeach
+
+                            </div>
+
+
+
+
+
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</div>
+
+@endsection
