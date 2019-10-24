a1b1f1cf2a7df4c600d30ec538f4da09966a2866
Razmy Mahsoom <razmyasdf@gmail.com>
2019-10-24 23:40:27 +0530

    Update ImageController.php

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -15,10 +15,40 @@ class ImageController extends Controller
         $img->img = $imagePath;
         $img->vehicle_vcn = $vehicle->vcn;
 
-        //dd(request('img'));
-        //dd(request('img')->store('uploads','public'));
+
         $img->save();
         return redirect()->back();
+
+    }
+
+    public function save(Request $request, Vehicle $vehicle){
+
+        request()->validate([
+
+            'img' => 'required',
+            'img.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048'
+        ]);
+
+        if ($image = $request->file('img')) {
+            foreach ($image as $files) {
+            $destinationPath = 'public/image/'; // upload path
+            $imagePath = $files->store('uploads','public');
+
+            $images[]=$imagePath;
+            }
+        }
+        foreach ($images as $imageP) {
+            $img = new image;
+            $img->img =$imageP;
+            $img->vehicle_vcn = $vehicle->vcn;
+
+            $img->save();
+        }
+       //dd($images);
+        //$img = new image;
+        //$img->img =$images;
+        //$img->vehicle_vcn = $vehicle->vcn;
+        //$img->save();
 
     }
 }
